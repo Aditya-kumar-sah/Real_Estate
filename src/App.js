7a8b97fb95ae1fe282399368ecdac34f0f6b7e27
MyFirstRepo <adityasaah932@gmail.com>
2024-09-19 21:36:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => {
   return <div className='max-w-[1440px] mx-auto bg-white'>
      <Header/>
      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/property/:id' element={<PropertyDetails/>} />
+        <Route path='/Real_Estate' element={<Home/>} />
+        <Route path='/Real_Estate/property/:id' element={<PropertyDetails/>} />
      </Routes>
      <Footer/>
   </div>;
