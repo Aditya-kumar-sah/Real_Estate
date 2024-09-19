a052eb5897ed19004746823bcd6ad60ec8870a57
MyFirstRepo <adityasaah932@gmail.com>
2024-09-19 20:59:09 +0530

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='py-6 px-4 mb-12 border-b flex justify-between items-center'>
+      <div className='container mx-auto '>
+         <Link className='text-4xl font-bold text-violet-500' to="/">HomeWay</Link>
+      </div>
+      <div className='flex items-center gap-6 '>
+         <Link className='hover:text-violet-900 tansition' to="">Login</Link>
+         <Link className='bg-violet-700 hover:bg-violet-800 text-white px-3 py-2 rounded-lg transition' to="">Signup</Link>
+      </div>
+  </header>;
 };
 
 export default Header;
